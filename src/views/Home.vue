4ba3c646a2df0f52fff2f08bdd20a0eb1fd93dab
ianfletcher55 <ian.fletcher55@gmail.com>
2020-07-07 22:01:55 -0500

    adds initial frontend app with CRUD actions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,109 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p></p>
+    <h1>Places</h1>
+
+    <ul>
+      <li v-for="error in errors">{{ error }}</li>
+    </ul>
+
+    <div>
+      Name: <input type="text" v-model="newPlaceName"> <br>
+      Address: <input type="text" v-model="newPlaceAddress"> <br>
+      <button v-on:click="createPlace()">Create Place</button>
+    </div>
+
+    <div v-for="place in places">
+      <h2>{{ place.name }}</h2>
+      <p>{{ "$" + place.address }}</p>
+      <button v-on:click="showPlace(place)">More Info</button>
+    </div>
+
+    <dialog id="place-details">
+      <form method="dialog">
+        <h1>Place Details</h1>
+        <p>Name: <input type="text" v-model="currentPlace.name"></p>
+        <p>Address: <input type="text" v-model="currentPlace.address"></p>
+        <button v-on:click="updatePlace(currentPlace)">Update</button>
+        <button v-on:click="destroyPlace(currentPlace)">Delete</button>
+        <button>Close</button>
+      </form>
+    </dialog>
+
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+</style>
 
+<script>
+import axios from "axios";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      places: [],
+      errors: [],
+      newPlaceName: "",
+      newPlaceAddress: "",
+      currentPlace: {}
+    };
+  },
+  created: function() {
+    this.indexPlaces();
+  },
+  methods: {
+    indexPlaces: function() {
+      axios.get("/api/places").then(response => {
+        console.log("All Places:", response.data);
+        this.places = response.data;
+      });
+    },
+    createPlace: function() {
+      var params = {
+        name: this.newPlaceName,
+        address: this.newPlaceAddress
+      };
+
+      axios
+        .post("/api/places", params)
+        .then(response => {
+          console.log("Success", response.data);
+          this.places.push(response.data);
+          this.newPlaceName = "";
+          this.newPlaceAddress = "";
+        })
+        .catch(error => {
+          console.log(error.response.data.errors);
+          this.errors = error.response.data.errors;
+        });
+    },
+    showPlace: function(place) {
+      console.log(place);
+      this.currentPlace = place;
+      document.querySelector("#place-details").showModal();
+    },
+    updatePlace: function(place) {
+      var updateParams = {
+        name: place.name,
+        address: place.address
+      };
+
+      axios
+        .patch(`/api/places/${place.id}`, updateParams)
+        .then(response => {
+          console.log("Successfully updated place", response.data);
+        })
+        .catch(error => {
+          console.log(error.response.data.errors);
+        });
+    },
+    destroyPlace: function(place) {
+      axios.delete(`/api/places/${place.id}`).then(respone => {
+        console.log("Successfully deleted place", respone.data);
+        var index = this.places.indexOf(place);
+        this.places.splice(index, 1);
+      });
+    }
   }
-}
+};
 </script>
